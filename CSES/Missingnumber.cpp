49a4721ae1181a30c3ce1c75f964c0d9f1bb84c7
FireFoxCEO <yihanrweng@gmail.com>
2025-03-18 10:22:00 -0500

    Update Missingnumber.cpp

--- CSES/Missingnumber.cpp
+++ CSES/Missingnumber.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() 
+{
+	int n; 
+    cin >> n;
+    vector<int> number(n);
+    for(int i = 0; i < n-1; i++)
+    {cin >> number[i];}
+
+    sort(number.begin(),number.end());
+    int x =0;
+    for(int i = 0; i < n; i++)
+    {
+        if(number[i] != i)
+        {
+            x = i;
+            cout << x;
+            break;
+        }
+    }
+    if(x == 0)
+    {
+        cout << n;
+    }
+}
