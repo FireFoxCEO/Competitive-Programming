9eabcc7f348bbb6b033e6e3cd3fde88470d2c47b
FireFoxCEO <yihanrweng@gmail.com>
2025-03-18 11:34:10 -0500

    Update Increasingarray.cpp

--- CSES/Introductory Problems/Increasingarray.cpp
+++ CSES/Introductory Problems/Increasingarray.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() 
+{
+	int n;
+    cin >> n;
+    vector<int> number(n);
+    long long x = 0;
+    for(int i =0; i < n; i++)
+    {
+        cin >> number[i];
+    }
+    for(int i = 0; i< n-1; i++)
+    {
+        if(number[i] > number[i+1])
+        {
+            x += number[i] - number[i+1];
+            number[i+1] += number[i] - number[i+1];
+        }
+    }
+    cout << x;
+}
