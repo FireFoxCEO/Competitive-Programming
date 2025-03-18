ced41a4f4b18f5d272508272feb1dfa766cea950
FireFoxCEO <yihanrweng@gmail.com>
2025-03-18 10:33:42 -0500

    Update Weirdalgorithm.cpp

--- CSES/Weirdalgorithm.cpp
+++ CSES/Weirdalgorithm.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() 
+{
+	int n;
+    cin >> n;
+    cout << n << " ";
+    while(n != 1)
+    {
+        if(n % 2 == 1 && n != 1)
+        {
+            n = 3 * n + 1;
+            cout<< n << " ";
+        }
+        else if(n % 2 == 0)
+        {
+            n /= 2;
+            cout << n << " ";
+        }
+    }
+}
